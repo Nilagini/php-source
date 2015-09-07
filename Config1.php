b4cbe30ecd3d4de5b774ada912555800fabd9975
reka <rthirunavukkarasu23@gmail.com>
2015-09-08 01:56:42 +0530

    credentials update

--- Config1.php
+++ Config1.php
@@ -2,6 +2,6 @@
  // Database configuration
     define('DB_USERNAME', 'adminGR2MJpj');
     define('DB_PASSWORD', '1ag4GNHNgL65');
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', getenv("OPENSHIFT_MYSQL_DB_HOST").':'.getenv("OPENSHIFT_MYSQL_DB_PORT"));
     define('DB_NAME', 'php');
 ?>
