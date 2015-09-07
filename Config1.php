cef938754074257435f64d81e08861013354fc45
reka <rthirunavukkarasu23@gmail.com>
2015-09-08 01:16:44 +0530

    pwd update

--- Config1.php
+++ Config1.php
@@ -2,6 +2,6 @@
  // Database configuration
     define('DB_USERNAME', 'a4516290_org');
     define('DB_PASSWORD', 'org123');
-    define('DB_HOST', 'http://mysql8.000webhost.com/');
+    define('DB_HOST', 'mysql8.000webhost.com');
     define('DB_NAME', 'a4516290_org');
 ?>
